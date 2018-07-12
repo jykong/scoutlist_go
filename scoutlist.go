ffdacae06cc32955a3602f9ee0e7474077e6e3b7
James Kong <jameskong@airfyer.local>
2018-07-12 12:37:00 -0400

    Added getPlaylists() user playlist printout

--- scoutlist.go
+++ scoutlist.go
@@ -7,6 +7,12 @@ import (
 	"github.com/zmb3/spotify"
 )
 
+// clientUser contains a client and userID
+type clientUser struct {
+	client *spotify.Client
+	userID string
+}
+
 func main() {
 	var client *spotify.Client
 	client = scoutlistAuth()
@@ -16,4 +22,21 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println("You are logged in as:", user.ID)
+
+	cu := clientUser{client, user.ID}
+	cu.getPlaylists("")
+}
+
+func (cu *clientUser) getPlaylists(file string) {
+	playlistsPage, err := cu.client.GetPlaylistsForUser(cu.userID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//total := playlistsPage.Total
+	//offset := playlistsPage.Offset
+	limit := playlistsPage.Limit
+	fmt.Println(playlistsPage.Total)
+	for i := 0; i < limit; i++ {
+		fmt.Println(playlistsPage.Playlists[i].ID, playlistsPage.Playlists[i].Name)
+	}
 }
