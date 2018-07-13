5db1b371e825b6809d61ecff80746d93aae770df
James Kong <jameskong@airfyer.local>
2018-07-13 05:11:56 -0400

    Added getUniqueTracksFromPlaylist(). Verified unique track filtering.

--- scoutlist.go
+++ scoutlist.go
@@ -14,6 +14,24 @@ type clientUser struct {
 	userID string
 }
 
+type playlistEntry struct {
+	ID   spotify.ID `json:"id"`
+	Name string     `json:"name"`
+}
+
+type playlistsContainer struct {
+	Playlists []playlistEntry `json:"items"`
+}
+
+type titleArtists struct {
+	Title   string
+	Artists []string
+}
+
+type tracksContainer struct {
+	tracksMap map[spotify.ID]titleArtists
+}
+
 const playlistsPath = "./playlists.json"
 const excPlaylistsPath = "./exc_playlists.json"
 
@@ -22,13 +40,18 @@ func main() {
 	cu.client = scoutlistAuth()
 	cu.getCurrentUserID()
 
-	var playlists playlistsContainer
-	cu.getPlaylists(&playlists)
-	savePlaylists(playlistsPath, &playlists)
+	//var playlists playlistsContainer
+	//cu.getPlaylists(&playlists)
+	//savePlaylists(playlistsPath, &playlists)
 
 	var excPlaylists playlistsContainer
 	loadPlaylists(excPlaylistsPath, &excPlaylists)
-	fmt.Println(excPlaylists)
+	//fmt.Println(excPlaylists)
+
+	var excTracks tracksContainer
+	cu.getUniqueTracksFromPlaylists(&excPlaylists, &excTracks)
+	fmt.Println(len(excTracks.tracksMap))
+	//fmt.Println(excTracks)
 }
 
 func (cu *clientUser) getCurrentUserID() {
@@ -38,15 +61,6 @@ func (cu *clientUser) getCurrentUserID() {
 	}
 	fmt.Println("You are logged in as:", user.ID)
 	cu.userID = user.ID
-}
-
-type playlistEntry struct {
-	ID   spotify.ID `json:"id"`
-	Name string     `json:"name"`
-}
-
-type playlistsContainer struct {
-	Playlists []playlistEntry `json:"items"`
 }
 
 func (cu *clientUser) getPlaylists(plCon *playlistsContainer) {
@@ -113,5 +127,90 @@ func loadPlaylists(filePath string, plCon *playlistsContainer) {
 	err = decoder.Decode(&plCon)
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func (cu *clientUser) getUniqueTracksFromPlaylists(
+	srcPlaylists *playlistsContainer, uniqueTracks *tracksContainer) {
+
+	log.Println("Getting unique tracks from playlists...")
+	var offset, limit int
+
+	var opt spotify.Options
+	opt.Offset = &offset
+	opt.Limit = &limit
+
+	fields := "items.track(id, name, artists.id), total"
+
+	var total int
+	var ta titleArtists
+
+	uniqueTracks.tracksMap = make(map[spotify.ID]titleArtists)
+
+	acc := 0
+	for _, pl := range srcPlaylists.Playlists {
+		for offset, limit, total = 0, 100, 100; offset < total; offset += limit {
+			plTrackPage, err := cu.client.GetPlaylistTracksOpt(cu.userID, pl.ID, &opt, fields)
+			if err != nil {
+				log.Fatal(err)
+			}
+			total = plTrackPage.Total
+			for _, tr := range plTrackPage.Tracks {
+				ta.Title = tr.Track.Name
+				ta.Artists = nil
+				for _, ar := range tr.Track.Artists {
+					ta.Artists = append(ta.Artists, ar.Name)
+				}
+				uniqueTracks.add(tr.Track.ID, &ta)
+			}
+		}
+		acc += total
+	}
+	log.Println("Done getting unique tracks.")
+	fmt.Println(acc)
+}
+
+func (uniqueTracks *tracksContainer) contains(tid spotify.ID, ta *titleArtists) bool {
+	_, present := uniqueTracks.tracksMap[tid]
+	if present == true {
+		return true
+	}
+	for _, uta := range uniqueTracks.tracksMap {
+		if ta.Title == uta.Title {
+			nArtists := len(ta.Artists)
+			if nArtists != len(uta.Artists) {
+				continue
+			}
+			uartists := make([]string, nArtists)
+			artists := make([]string, nArtists)
+			copy(uartists, uta.Artists)
+			copy(artists, ta.Artists)
+			for match := true; match == true; {
+				match = false
+				for i, uartist := range uartists {
+					for j, artist := range artists {
+						if artist == uartist {
+							uartists = append(uartists[:i], uartists[i+1:]...)
+							artists = append(artists[:j], artists[j+1:]...)
+							match = true
+							break
+						}
+					}
+					if match {
+						break
+					}
+				}
+			}
+			if len(uartists) == 0 && len(artists) == 0 {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (uniqueTracks *tracksContainer) add(tid spotify.ID, ta *titleArtists) {
+	if uniqueTracks.contains(tid, ta) == false {
+		uniqueTracks.tracksMap[tid] = *ta
 	}
 }
